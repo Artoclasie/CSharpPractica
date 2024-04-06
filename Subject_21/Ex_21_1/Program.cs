08d400d9b49e62a831d925d0f6b6d731aa1fdfc2
PC <PC@LAPTOP-QVI7ULCL>
2024-04-06 18:35:35 +0300

    Добавили тему 21

---
+++ Subject_21/Ex_21_1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex_21_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ChocolateBoiler boiler = ChocolateBoiler.GetInstance();
+
+            boiler.Fill();
+
+            bool isEmpty = boiler.IsEmpty();
+            Console.WriteLine("Бойлер заполнен: " + isEmpty);
+
+            boiler.Drain();
+
+            isEmpty = boiler.IsEmpty();
+            Console.WriteLine("Бойлер заполнен: " + isEmpty);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Subject_21/Ex_21_1/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_21_1
+{
+    internal class Singleton
+    {
+        private volatile static Singleton uniqueInstance;
+        private static readonly object lockObject = new object();
+        private Singleton() { }
+
+        public static Singleton getInstance
+        {
+            get
+            {
+                lock (lockObject)
+                {
+                    if (uniqueInstance == null)
+                    {
+                        uniqueInstance = new Singleton();
+                    }
+                    return uniqueInstance;
+                }
+            }
+        }
+    }
+}
